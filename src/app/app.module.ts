c948330c0caaec74c966a481f0899a94e8631f1e
Misha Hrynko <misha@mate.academy>
2023-03-30 20:07:06 +0300

    after Code Splitting and Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { FilterActivePipe } from './pipes/filter-active.pipe';
 import { MessageComponent } from './components/message/message.component';
 import { TodosPageComponent } from './pages/todos-page/todos-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'todos', component: TodosPageComponent },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then(m => m.AboutModule)
+  },
+  { path: '**', redirectTo: '/todos', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +32,7 @@ import { TodosPageComponent } from './pages/todos-page/todos-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
